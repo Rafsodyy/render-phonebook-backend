f14009260352003d89fdc6325584275b52366fac
Rafsodyy <111101412+Rafsodyy@users.noreply.github.com>
2024-05-03 14:24:21 +0800

    Update

--- index.js
+++ index.js
@@ -1,97 +1,117 @@
-require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
+const morganBody = require('morgan-body')
 const app = express()
-const morganBody = require ('morgan-body')
-const Person = require('./models/person')
+const cors = require('cors')
 
-//cross origin middleware for deployement 
 app.use(cors())
-//Parser first then followed by morganBody
 app.use(express.json())
-//morgan hook for the app
-morganBody(app)
-//Custom app use token to log using morgan
+app.use(express.static('dist'))
+// morganBody(app)
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-//Use the dist production build front-end
-app.use(express.static('dist'))
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 
-
-
-const generateDate = () => {
-  const date = new Date();
-  let fullDate = date.toString();
-  return fullDate
-}
+// const generateId = () => {
+// 	const maxId = persons.length > 0
+// 	? Math.max(...persons.map(p => p.id))
+// 	: 0
+// 	return maxId + 1
+// }
 
 const generateId = () => {
-  // const matchingId = persons.filter(person => p.id)
-  const newId =  Math.floor(Math.random() * 10000);
-  return newId
-}
+	// const matchingId = persons.filter(person => p.id)
+	const newId =  Math.floor(Math.random() * 10000);
+	return newId
+  }
 
-app.get('/api/persons', (request, response) => {
-	Person.find({})
-    .then (person => {
-      response.json(person)
-    })
-})
+const generateDate = () => {
+	const date = new Date();
+	let fullDate = date.toString();
+	return fullDate
+  }
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for 2 people</p><p>${generateDate()}</p>`)
+	response.send(`<p>Phonebook has info for 2 people </p><p>${generateDate()}</p>`)
+  })
+
+app.get('/api/persons', (request, response) => {
+	response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if(person){
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+	const personId = Number(request.params.id)
+	const person = persons.find(person => person.id === personId)
+	response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  // const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+	const personId = Number(request.params.id)
+	persons = persons.filter(person => person.id !== personId)
+	response.status(404).end()
 })
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
-  const nameIsExisting = persons.find(person => person.name === body.name) ? true : false
-  console.log(nameIsExisting)
-  //if else Guard
-  if(nameIsExisting === true){
-      return response.status(404).json({
-        error: "Name must be unique"
-      })
-    }
-  if(!body.name || !body.number){
-    return response.status(404).json({
-      error: "Name or number is missing"
-    })
-  }
+	const body = request.body
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number
-  }
+	const nameIsExisting = persons.find(person => person.name === body.name) ? true : false
+	if(nameIsExisting === true){
+		return response.json({
+			error: "Name is already existing"
+		})
+	}
 
-  persons = persons.concat(person)
-  response.json(person)
+	if(!body.name){
+		return response.status(400).json({
+			error: 'Name is missing'
+		})
+	}
+	if(!body.number){
+		return response.status(404).json({
+			error: 'Number is missing'
+		})
+	}
 
-  morgan.token('body', req => {
-    return JSON.stringify(req.body)
-  })
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number
+	}
+	persons = persons.concat(person)
+
+	// morgan.token('body', request => {
+	// 	return JSON.stringify(request.body)
+	// })
 
 })
 
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint'})
+  }
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT
-app.listen( PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
